1bf1c4ea3f092ebc83313bdaeaecff84f24bd047
Lapoozza <jewelmahanta@gmail.com>
2016-07-24 19:04:26 +0530

    Completed lesson 8

---
+++ Basics of Programming with JavaScript/lesson8.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 24-07-2016.
+ */
+
+/* Conditional Statements
+    Important list to remember.
+    In Javascript, the following values 'only' are falsy (as in they are evaluated to false when we pass them
+    to a conditional statement)
+
+    0 -0 NaN false null undefined ""
+ */
+
+var a;
+a = 0;
+
+if(a){
+    var b = 4;
+    b *= 2;
+    console.log(b);
+}
+
+console.log(void 43);
