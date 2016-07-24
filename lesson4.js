4bfaa15405c4204c64e4bf431acffeb34fbead41
Lapoozza <jewelmahanta@gmail.com>
2016-07-24 12:25:58 +0530

    Completed lesson4 (Input and Output with JS)

---
+++ lesson4.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 24-07-2016.
+ */
+
+/* Input / Output
+   ---------------
+
+   Input be done using 'prompt();'
+   example: prompt('Input your age?');
+
+   Output can be done using 'alert();'
+   example: alert('This is a sample alert');
+
+   TODO: figure out how to do alert and prompt with WebStorm
+ */
