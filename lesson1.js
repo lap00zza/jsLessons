f47d43aeeacbb37d022838cf5bb4d8427c604d6d
Lapoozza <jewelmahanta@gmail.com>
2016-07-24 10:55:59 +0530

    Statements lesson complete

--- lesson1.js
+++ lesson1.js
@@ -1,3 +1,26 @@
 /**
- * Created by dell on 24-07-2016.
+ * Created by Jewel Mahanta on 24-07-2016.
  */
+// Statements
+// ----------
+// In a computer language, a group of words, numbers, and operators that performs a specific task is a statement.
+// In JavaScript, a statement might look as follows:
+
+// a = b * 2;
+
+// The characters a and b are called variables (see "Variables"), which are like simple boxes you can store any of
+// your stuff in. In programs, variables hold values (like the number 42) to be used by the program. Think of them
+// as symbolic placeholders for the values themselves. By contrast, the 2 is just a value itself, called a literal
+// value, because it stands alone without being stored in a variable. The = and * characters are operators
+// (see "Operators") -- they perform actions with the values and variables such as assignment and mathematic
+// multiplication. Most statements in JavaScript conclude with a semicolon (;) at the end.
+//
+// The statement a = b * 2; tells the computer, roughly, to get the current value stored in the variable b,
+// multiply that value by 2, then store the result back into another variable we call a.
+
+// Programs are just collections of many such statements, which together describe all the steps that it takes to
+// perform your program's purpose.
+
+b = 20;
+a = b * 20;
+console.log(a);
