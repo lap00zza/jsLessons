02f67574d25eeaa9281536456a5194dc8547d903
Lapoozza <jewelmahanta@gmail.com>
2016-07-24 18:55:11 +0530

    Completed lesson 7

---
+++ Basics of Programming with JavaScript/lesson7.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 24-07-2016.
+ */
+/* Variables and Blocks
+
+ */
+
+var a;
+a = 42;
+const b = 10;
+console.log(a, b);
+console.log(typeof a, typeof b);
+
+function d() {
+    console.log(">.<")
+
+}
+
+if (a<10){
+    console.log("So cool \\o/");
+}
+
+while (a > 1){
+    console.log(a);
+    a /= 2;
+}
+
+d();
