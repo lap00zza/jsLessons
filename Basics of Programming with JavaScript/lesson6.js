b5a6e62608a38920a1a564135ee9c0108c34a6cc
Lapoozza <jewelmahanta@gmail.com>
2016-07-24 18:29:17 +0530

    Completed lesson 6

---
+++ Basics of Programming with JavaScript/lesson6.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 24-07-2016.
+ */
+
+/* Values and Types and Code Comments [Combined]
+
+ */
+a = "2";
+b = 2;
+console.log(a, b);
+
+c = 2;
+console.log(typeof c);
+
+c = String(c);
+console.log(typeof c);
+
+d = 1.234 + "..." + 3 + "...";
+console.log(d);
+
+// Single line comment
+/*
+    Multi
+    Line
+    Comment
+ */
+
+// Remember a comment should never say what a code is saying. It should only say why and / or how
