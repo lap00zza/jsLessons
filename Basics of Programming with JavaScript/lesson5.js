a2a59b18451c2bdc08a97beb3e5320f7773dfb23
Lapoozza <jewelmahanta@gmail.com>
2016-07-24 18:08:47 +0530

    1. Completed lesson5 2. Moved the lessons inside a sub directory for easier grouping

---
+++ Basics of Programming with JavaScript/lesson5.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 24-07-2016.
+ */
+
+/* Operators
+ Here are some of the most common operators in JavaScript:
+
+ - Assignment: = as in a = 2.
+ - Math: + (addition), - (subtraction), * (multiplication), and / (division), as in a * 3.
+ - Compound Assignment: +=, -=, *=, and /= are compound operators that combine a math operation with
+   assignment, as in a += 2 (same as a = a + 2).
+ - Increment/Decrement: ++ (increment), -- (decrement), as in a++ (similar to a = a + 1).
+ - Object Property Access: . as in console.log().
+
+ Objects are values that hold other values at specific named locations called properties. obj.a means an
+ object value called obj with a property of the name a. Properties can alternatively be accessed as obj["a"].
+ See Chapter 2.
+
+ Equality: == (loose-equals), === (strict-equals), != (loose not-equals), !== (strict not-equals), as in a == b.
+
+ Comparison: < (less than), > (greater than), <= (less than or loose-equals), >= (greater than or loose-equals),
+ as in a <= b.
+
+ Logical: && (and), || (or), as in a || b that selects either a or b.
+
+ */
+d = 0;
+a = 2;
+a = a + 2;
+c =+ 2;
+d -= 2;
+console.log(a, c, d);
