659250b363cd5c667b6e3650577c3032bdda64d4
Lapoozza <jewelmahanta@gmail.com>
2016-07-27 12:59:15 +0530

    Completed functions and learned about creating JS file from scratch

---
+++ Quick Start to JavaScript Volume 2/lesson5.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 27-07-2016.
+ */
+
+/* Arrays
+
+ */
+
+var cool = ["a", "b", "c", "d", "e"];
+cool[10] = "laugh";
+function coolYo() {
+    for(var i = 0; i < cool.length; i++){
+        console.log(cool[i]);
+
+    }
+}
+coolYo();
