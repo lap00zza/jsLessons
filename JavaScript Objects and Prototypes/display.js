981e751e891d20930657982ae9d5be771d1c0c9d
Lapoozza <jewelmahanta@gmail.com>
2016-07-30 20:47:32 +0530

    Intermediate lesson 1

---
+++ JavaScript Objects and Prototypes/display.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 30-07-2016.
+ */
+
+function displayValue(value, addMargin, addPadding) {
+    "use strict";
+    var div = document.createElement('div');
+    div.style.fontSize = '32px';
+    if (addMargin) {
+        div.style.marginBottom = '30px';
+    }
+    if (addPadding) {
+        div.style.paddingLeft = '30px';
+    }
+    div.textContent = value;
+    document.body.appendChild(div);
+}
+
+/*function getTypeName(object) {
+    "use strict";
+    var results = (/function (.{1,})\(/).exec(object.constructor.toString());
+    return (results && results.length > 1) ? results[1] : "";
+}*/
+
+function get_type(thing) {
+    "use strict";
+    if (thing === null) {
+        return "[object Null]";
+    } // special case
+    window.console.log(thing.constructor.name);
+    // return Object.prototype.toString.call(thing);
+    return thing.constructor.name;
+}
+
+function displayObject(object) {
+    "use strict";
+    var propertyName;
+    if (object === null) {
+        displayValue('null');
+    }
+    displayValue(get_type(object) + ' {');
+    for (propertyName in object) {
+        if (propertyName !== 'constructor') {
+            displayValue(propertyName + ': ' + object[propertyName], false, true);
+        }
+    }
+    displayValue('}', true);
+}
+
+function display() {
+    "use strict";
+    var i;
+    for (i = 0; i < arguments.length; i += 1) {
+        if (typeof arguments[i] === 'object') {
+            displayObject(arguments[i]);
+        } else {
+            displayValue(arguments[i], true);
+        }
+    }
+}
+
---
+++ JavaScript Objects and Prototypes/script.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Jewel Mahanta (@lap00zza) on 30-07-2016.
+ */
+/*global display
+
+ */
+/*display("Hello World");
+
+var cat = {"name": "Alex", "type": "persian"};
+display(cat);*/
+
+function Cat(name, color) {
+    "use strict";
+    this.name = name;
+    this.color = color;
+}
+
+var nice = new Cat("Joanita", "Brown");
+display(nice);
+/*var lapoozza = Object.create(Object.prototype, {
+    name : {
+        value: "Lapoozza",
+        enumerable: true,
+        writable: true,
+        configurable: true
+    },
+    color : {
+        value: "Black",
+        enumerable: true,
+        writable: true,
+        configurable: true
+    }
+});*/
+var hello = Object.create(Cat, {
+    name: {
+        value: "Lapoozza",
+        // writable: true,
+        enumerable: true
+        // configurable: true
+    },
+    color: {
+        value: "Blond",
+        // writable: true
+        enumerable: true
+        // configurable: true
+    }
+});
+display(hello);
